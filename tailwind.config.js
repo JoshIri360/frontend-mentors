5e3641f36f6b3edd495ad603f4873808c0c1859a
Josh <aidelojejoshua@gmail.com>
2022-05-21 15:41:37 +0100

    finished 3rd project and audited

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,7 @@ module.exports = {
 	theme: {
 		fontFamily: {
 			poppins: ["poppins"],
+			Outfit: ["Outfit"],
 		},
 		extend: {
 			colors: {
@@ -19,6 +20,11 @@ module.exports = {
 				dBlue: "hsl(213, 19%, 18%)",
 				tBlue: "hsl(213, 19%, 28%)",
 				vdBlue: "hsl(216, 12%, 8%)",
+				pr3softBlue: "hsl(215, 51%, 70%)",
+				pr3cyan: "hsl(178, 100%, 50%)",
+				pr3mainBG: "hsl(217, 54%, 11%)",
+				pr3line: "hsl(215, 32%, 27%)",
+				pr3cardBG: "hsl(216, 50%, 16%)",
 			},
 		},
 	},
